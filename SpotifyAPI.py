4cb294daafd3364f7216c2c25b507be153634697
Tyler-Culp <tculp@ucsd.edu>
2022-06-19 16:13:33 -0700

    More API practice

--- SpotifyAPI.py
+++ SpotifyAPI.py
@@ -1,12 +1,13 @@
 import requests
 import json
 
-response = requests.get('https://api.spotify.com/v1')
+response = requests.get('https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy')
 #response = requests.get('https://api.covid19india.org/state_district_wise.json')
 print(response.status_code)
-data = response.text
-parsed = json.loads(data)
+print(response.content)
+#data = response.text
+#parsed = json.loads(data)
 
-print(data)
+#print(data)
 
 
---
+++ firstAPI.py
@@ -0,0 +1,20 @@
+import requests
+
+response = requests.get("https://api.thedogapi.com/v1/breeds/1")
+
+request = response.request
+
+#print(response.headers.get("Content-Type"))
+# print(response.json())
+# print(response.json()["name"])
+
+headers = {"X-Request-Id": "<my-request-id>"}
+response2 = requests.get("https://example.org", headers=headers)
+
+
+response3 = requests.get("https://api.lorem.space/image/game?w=150&h=220")
+print(response3.headers.get("Content-Type"))
+
+file = open("video-game.jpeg", "wb")
+file.write(response.content)
+file.close()
