919b1a35bdab3975f535429f6a20bdb5d086ce4b
Tyler-Culp <tculp@ucsd.edu>
2022-06-19 14:55:17 -0700

    Update SpotifyAPI.py

--- SpotifyAPI.py
+++ SpotifyAPI.py
@@ -1,1 +1,12 @@
+import requests
+import json
+
+response = requests.get('https://api.spotify.com/v1')
+#response = requests.get('https://api.covid19india.org/state_district_wise.json')
+print(response.status_code)
+data = response.text
+parsed = json.loads(data)
+
+print(data)
+
 
