b285ec0573015fd57eecc6365ef74c91ae257453
Tyler-Culp <tculp@ucsd.edu>
2022-06-19 22:14:55 -0700

    update

--- firstAPI.py
+++ firstAPI.py
@@ -13,8 +13,22 @@ response2 = requests.get("https://example.org", headers=headers)
 
 
 response3 = requests.get("https://api.lorem.space/image/game?w=150&h=220")
-print(response3.headers.get("Content-Type"))
+# print(response3.headers.get("Content-Type"))
 
-file = open("video-game.jpeg", "wb")
-file.write(response3.content)
-file.close()
+# file = open("video-game.jpeg", "wb")
+# file.write(response3.content)
+# file.close()
+
+query_params = {"gender": "female", "nat": "de"}
+randomUser = requests.get("https://randomuser.me/api/", params = query_params).json()
+# randomUser = requests.get("https://randomuser.me/api/?gender=female&nat=de").json()
+
+
+#print(randomUser)
+
+
+endpoint = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos"
+Nasa_api_key = "DEMO_KEY"
+Nasa_query_params = {"api_key": Nasa_api_key, "earth_date": "2020-07-01"}
+Nasa_response = requests.get(endpoint, params=query_params)
+print(Nasa_response)
